8ee667559c74cfcd3cf819cfe6397f57a5266aaa
Sebastián Rubio Quiroz <sebastian.rubio@iest.edu.mx>
2023-05-21 19:59:56 -0600

    Fix sass

--- frontend/src/pages/PerfilAspirante/index.js
+++ frontend/src/pages/PerfilAspirante/index.js
@@ -16,18 +16,18 @@ const PerfilAspirante = () => {
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
-      <div className='profile-container'>
-        <div className='profile-container__box--info'>
-          <p className='profile-container__box--info--bold'>Mi perfil</p>
+      <div className='profile-container2'>
+        <div className='profile-container2__box--info'>
+          <p className='profile-container2__box--info--bold'>Mi perfil</p>
           <img alt='profile-pic' src={localStorage.getItem('imageUrl')} />
-          <p className='profile-container__box--info--bold'>Aspirante</p>
+          <p className='profile-container2__box--info--bold'>Aspirante</p>
           <p>
             {localStorage.getItem('name')} {localStorage.getItem('lastNames')}
           </p>
-          <p className='profile-container__box--info--bold'>Carrera</p>
+          <p className='profile-container2__box--info--bold'>Carrera</p>
           <p>{localStorage.getItem('bachelor')}</p>
         </div>
-        <div className='profile-container__box--points'>
+        <div className='profile-container2__box--points'>
           <h1>Actualizaciones sobre tu solicitud:</h1>
         </div>
       </div>
