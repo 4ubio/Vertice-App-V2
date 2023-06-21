f58ed4da8f7ff25471dfff184167203dba454b31
Sebastián Rubio Quiroz <sebastian.rubio@iest.edu.mx>
2023-06-20 22:53:00 -0600

    Change server url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 //Initialize our API to request the info
 const api = axios.create({
-  //baseURL: 'https://vertice-app-v2-2kfzta9mp-4ubio.vercel.app/',
-  baseURL: 'http://localhost:8080/',
+  baseURL: 'https://vertice-app-v2.vercel.app/',
+  //baseURL: 'http://localhost:8080/',
 });
 
 export default api;
