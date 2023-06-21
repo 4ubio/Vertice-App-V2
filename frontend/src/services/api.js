1e2369e10e1efa0e96b457248e350bfbd9b57fa7
Sebastián Rubio Quiroz <sebastian.rubio@iest.edu.mx>
2023-06-20 22:07:25 -0600

    Vercel backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Initialize our API to request the info
 const api = axios.create({
-  // baseURL: 'https://shielded-chamber-97646.herokuapp.com/',
+  baseURL: 'https://shielded-chamber-97646.herokuapp.com/',
   baseURL: 'http://localhost:8080/',
 });
 
