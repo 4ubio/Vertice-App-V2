0a8f37698a82da183613b3c95584a758074442a1
Josue Arreola <josue@heroguest.com>
2023-08-08 20:43:56 -0600

    Add about devs section

--- frontend/src/pages/LandingPage/index.js
+++ frontend/src/pages/LandingPage/index.js
@@ -2,12 +2,10 @@ import React, { useState } from "react";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import Sidebar from "../../components/Sidebar";
-import header from "../../images/header.png"
+import header from "../../images/header.png";
 import verticeImg from "../../images/vertice2.png";
-import sebas from "../../images/sebas.png";
-import josue from "../../images/josue.png";
-import mafer from "../../images/mafer.png";
-import david from "../../images/david.png";
+import iestLogo from "../../images/logoIest.webp";
+import devs from "../../images/devs.png";
 
 import "./LandingPage.scss";
 
@@ -34,52 +32,68 @@ const LandingPage = () => {
       <div>
         <div className="about">
           <div className="p-4">
-            <h2 className="vertice_title">¿Qué es <br></br> <span className="black">Vértice</span>?</h2>
+            <h2 className="vertice_title">
+              ¿Qué es <br></br> <span className="black">Vértice</span>?
+            </h2>
             <p className="lh-2 vertice_subtitle">
-              Es el Programa de Excelencia de la Red de Universidades Anáhuac el cual complementa tus estudios a través de una experiencia de formación multidisciplinaria, que te permitirá consolidarte como líder de excelencia capaz de transformar tu vida y nuestro entorno.
+              Es el Programa de Excelencia de la Red de Universidades Anáhuac el
+              cual complementa tus estudios a través de una experiencia de
+              formación multidisciplinaria, que te permitirá consolidarte como
+              líder de excelencia capaz de transformar tu vida y nuestro
+              entorno.
             </p>
           </div>
 
           <div className="img_container">
-            <img className="vertice-img" src={verticeImg}/>
+            <img className="vertice-img" src={verticeImg} />
           </div>
         </div>
 
-        <div className="p-4">
-          <div className="card card--gray">
-            <h2>Historia de desarrollo del proyecto</h2>
-            <p className="txt-justify">
-              A lo largo de los años, la tecnología ha evolucionado de manera que
-              nos es más sencillo el realizar aquellas actividades que
-              anteriormente solo se realizaban a mano, por lo que después de
-              juntas, borradores de ideas, modificaciones, etc. Se ha desarrollado
-              Vértice App, la cual ayudará a los alumnos, maestros y
-              administradores, a llevar un control eficaz y al alcance de sus
-              manos con solo buscar la página. Siendo esta la segunda versión, en
-              la que se llevará a cabo un control más eficiente y sencillo, con la
-              existencia de tres distintos perfiles, los cuales son: No Miembros,
-              Miembros y Administradores, esto para evitar la confusión de proceso
-              de admisión, puntos alcanzados semestralmente, dadas de baja, etc.
-              Este sitio web, no existiría si no fuera por los alumnos de la
-              carrera de Ing. En Sistemas y Negocios Digitales, División de
-              Ciencias Exactas, pues ellos son los que se han encargado de llevar
-              a cabo todo el proyecto, iniciando con la idea y el proceso las
-              alumnas, Teresa Lisette Rico Soto y Andrea Sosa Salazar. Dándole
-              seguimiento a la versión actual los alumnos Sebastian Rubio Quiroz,
-              Josue David Arreola Aguilera, María Fernanda Villarreal Mar y David
-              Alejandro Rivera Luna. Con el apoyo de los docentes Victor Guillermo
-              Asad Zetina y José Luis Martínez Hernández, debido a que brindaron
-              las bases y las herramientas para hacer posible este gran proyecto.
-            </p>
+        <div className="about-devs">
+          <div className="p-4">
+            <div className="about-devs-card">
+              <h2 className="">Desarrollo del proyecto</h2>
+              <p>
+                Desarrollar una aplicación web progresiva para el Programa de
+                Excelencia Vértice IEST Anáhuac que brinde soporte en la
+                planeación, control de las actividades, eventos, control de
+                asistencia y visualización del avance curricular, de manera que
+                se brinde un mejor servicio a los miembros del programa.
+              </p>
+              <p>
+                Bajo la dirección de los maestros Victor Guillermo Asad Zetina y
+                José Luis Martínez Hernández de la División de Ciencias Exactas
+                del IEST - ANÁHUAC.
+              </p>
+              <div className="center">
+                <img width={280} src={iestLogo} />
+              </div>
+            </div>
           </div>
-          <div className="developers mt-4">
-            <img height={250} src={sebas} alt="Sebastia Rubio" />
-            <img height={250} src={josue} alt="Josué Arreola" />
-            <img height={250} src={mafer} alt="Maria Fernanda Villarreal" />
-            <img height={250} src={david} alt="David Rivera" />
+          <div className="p-4">
+            <div className="color-gray about-devs-card-2">
+              <h2 className="center">
+                Iniciativa de dos alumnas del programa de Excelencia Vértice
+              </h2>
+              <div className="center">
+                <img width={400} src={devs} />
+              </div>
+              <h2 className="center">Andrea Sosa &ensp; &  &ensp; Teresa Rico</h2>
+              <div className="about-devs-card-3">
+                <p className="center">
+                  Segunda fase del proyecto integrado por el equipo de alumnos
+                  de la carrera de Ing. Sistemas y Negocios Digitales.
+                </p>
+                <ul>
+                  <li>Sebastián Rubio Quiroz</li>
+                  <li>Josué David Arreola Aguilera</li>
+                  <li>María Fernanda Villarreal Mar</li>
+                  <li>David Alejandro Rivera Luna</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-        
       </div>
 
       <Footer />
